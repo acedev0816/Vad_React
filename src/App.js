0f6eb9441fa5f12873f545b620feabb73628551a
acedev0816 <acedev0816@gmail.com>
2024-09-25 02:42:11 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -42,20 +42,18 @@ const App = () => {
               version="1.1"
               xmlns="http://www.w3.org/2000/svg"
             >
-              {" "}
               <g
                 id="Page-1"
                 stroke="none"
-                stroke-width="1"
+                strokeWidth="1"
                 fill="none"
-                fill-rule="evenodd"
+                fillRule="evenodd"
               >
-                {" "}
                 <path
                   d="M7,12 C8.66,12 9.99,10.66 9.99,9 L10,3 C10,1.34 8.66,0 7,0 C5.34,0 4,1.34 4,3 L4,9 C4,10.66 5.34,12 7,12 Z M12.3,9 C12.3,12 9.76,14.1 7,14.1 C4.24,14.1 1.7,12 1.7,9 L0,9 C0,12.41 2.72,15.23 6,15.72 L6,19 L8,19 L8,15.72 C11.28,15.24 14,12.42 14,9 L12.3,9 Z"
                   id="mic"
                   fill="#FFFFFF"
-                  fill-rule="nonzero"
+                  fillRule="nonzero"
                 ></path>{" "}
               </g>{" "}
             </svg>
@@ -65,7 +63,7 @@ const App = () => {
         </div>
 
         <p> {vad.loading && "Loading"}</p>
-        <p> {vad.errored && "Errored"}</p>
+        <p> {vad.errored && vad.errored.message}</p>
         <p> {recording && vad.userSpeaking ?  "User is speaking" : "Silence"}</p>
       </header>
     </div>
